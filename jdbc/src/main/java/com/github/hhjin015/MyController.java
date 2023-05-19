2bb548ea4ba41e71d6768b84180aad0cbf9d5aa7
dhslrl321 <dhslrl321@gmail.com>
2023-05-19 21:22:01 +0900

    add last study

--- jdbc/src/main/java/com/github/hhjin015/MyController.java
+++ jdbc/src/main/java/com/github/hhjin015/MyController.java
@@ -1,7 +1,16 @@
 package com.github.hhjin015;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
+
+    @GetMapping("/hell")
+    public String a() {
+        ThreadLocal<Integer> tl  = new InheritableThreadLocal<>();
+        tl.set(10);
+        return Thread.currentThread().getName();
+    }
+
 }
--- jdbc/src/test/java/com/github/ConnectionTest.java
+++ jdbc/src/test/java/com/github/ConnectionTest.java
@@ -1,5 +1,8 @@
 package com.github;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.sql.*;
@@ -10,18 +13,47 @@ public class ConnectionTest {
     private final static String USERNAME = "sa";
     private final static String PASSWORD = "";
 
-    @Test
-    void name() throws SQLException {
+    @BeforeEach
+    void setUp() throws Exception {
         Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         final PreparedStatement statement = connection.prepareStatement("DROP TABLE IF EXISTS member CASCADE; " +
                 "CREATE TABLE member (id bigint , name varchar(255), primary key (id));");
         statement.execute();
+    }
 
+    @Test
+    void a2() throws SQLException {
         Connection connection2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         PreparedStatement ps2 = connection2.prepareStatement("SELECT count(*) FROM member;");
         ResultSet rs = ps2.executeQuery();
         rs.next();
         int anInt = rs.getInt(1);
+        System.out.println("anInt = " + anInt);
+    }
+
+    @Test
+    @DisplayName("insert 가능")
+    void a3() throws SQLException {
+        Connection c1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        PreparedStatement ps = c1.prepareStatement("INSERT INTO member (id, name) VALUES (1, 'jang')");
+        ps.execute();
+    }
+
+    @Test
+    @DisplayName("select 가능")
+    void a4() throws SQLException {
+        Connection c1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        PreparedStatement ps = c1.prepareStatement("INSERT INTO member (id, name) VALUES (1, 'jang')");
+        ps.execute();
+
+        Connection c2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        ResultSet rs = c2.prepareStatement("SELECT * FROM member").executeQuery();
+        rs.next();
+
+        int anInt = rs.getInt(1);
+        String string = rs.getString(2);
+
+        System.out.println("string = " + string);
         System.out.println("anInt = " + anInt);
     }
 }
